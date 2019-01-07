4ff30fef4c1f1f5f226bb3999057e54f9b77d19c
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-07 01:39:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,9 +85,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.collect do |holiday, supplies|
-    if supplies.include?("BBQ")
-      holiday
+  holiday_hash.each do |season, holidays|
+    holidays.collect do |holiday, supplies|
+      if supplies.include?("BBQ")
+        holiday
+      end
     end
   end
 
