6bd0b0572454fa5d146773d06e822ef1d88cd1fb
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-07 01:19:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,8 +71,8 @@ def all_supplies_in_holidays(holiday_hash)
             end
           end
         end
-        key = key.to_s.split('_').each {|word| word.capitalize}
-        
+        key = key.to_s.split('_').collect {|word| word.capitalize}
+
         puts "  #{key.join(' ')}: #{supplies}"
       end
   end
